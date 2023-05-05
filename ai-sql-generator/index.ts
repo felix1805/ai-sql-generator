396eb894c1c8021570bad34e93412f82cc44d254
felix1805 <felix1805@gmail.com>
2023-05-05 17:32:58 -0400

    fixed server connection issues by switching to previous openAI model

--- ai-sql-generator/index.ts
+++ ai-sql-generator/index.ts
@@ -6,7 +6,7 @@ const PORT: number = 8000
 const app: Application = express()
 app.use(cors())
 app.use(express.json())
-const API_KEY: string = "*********"
+const API_KEY: string = '--------'
 const configuration = new Configuration({
   apiKey: API_KEY
 })
@@ -16,7 +16,7 @@ const openai = new OpenAIApi(configuration)
 app.post("/completions", async (req: Request, res: Response) => {
   try {
     const completion = await openai.createChatCompletion({
-      model: "gpt-4",
+      model: "gpt-3.5-turbo",
       messages: [
         {
           role: "user",
