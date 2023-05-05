84c7afc9fb60e91d59b82e8b651dcae3c5c8ba55
felix1805 <felix1805@gmail.com>
2023-05-05 17:27:26 -0400

    completed index.ts file, still working to fix server errors

--- ai-sql-generator/src/components/MessagesDisplay.tsx
+++ ai-sql-generator/src/components/MessagesDisplay.tsx
@@ -4,6 +4,10 @@ const MessagesDisplay = () => {
   return (
     <div className="messages-display">
       <MessageDisplay/>
+      <MessageDisplay/>
+      <MessageDisplay/>
+      <MessageDisplay/>
+      <MessageDisplay/>
     </div>
   );
 }
